b619c15cdac15a85380aba63d5aea96e949b31ae
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2024-10-11 18:24:52 -0500

    list  categories first version

--- scripts/listCategories.js
+++ scripts/listCategories.js
@@ -3,11 +3,18 @@ const { PATH } = require("../constants/app");
 
 function listCategories() {
   const categories = fs.readdirSync(PATH.DOCS);
+  let categoriesList = [];
 
   if (categories.length > 0) {
     categories.forEach((category) => {
-      console.log(category);
+      let path = `${PATH.DOCS}/${category}/README.md`;
+      let file = fs.readFileSync(path, "utf-8");
+      categoriesList.push({
+        key: category,
+        value: file.split("\n")[0].split("#")[1].trim(),
+      });
     });
+    console.log(categoriesList);
   }
 }
 
