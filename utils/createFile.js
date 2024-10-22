20e8950c6a52293e9b86d9109e247ab7815df2b3
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2024-10-22 00:00:33 -0600

    fix path join

--- utils/createFile.js
+++ utils/createFile.js
@@ -1,11 +1,12 @@
 import fs from "fs";
 import { validateFolder } from "../utils/validateFolder.js";
+import path from "path";
 
 export function createFile(folderPath, fileName, fileContent) {
   let isFileCreated = false;
   if (validateFolder(folderPath)) {
     try {
-      const filePath = `${folderPath}/${fileName}`;
+      const filePath = path.join(folderPath, fileName);
       fs.writeFileSync(filePath, fileContent);
       isFileCreated = true;
     } catch (err) {
